8302843c8e9af199fcca1d765abaad6334a96c7f
Joseph Lejtman <lejtmany@gmail.com>
2015-11-04 22:55:05 -0500

    changed to asyncio model

--- client.py
+++ client.py
@@ -1,11 +1,31 @@
-import socket
+import asyncio
 
-s = socket.socket()
-host = 'localhost'
-ip = socket.gethostbyname(host)
-s.connect((ip, 8888))
-print('connected to {}'.format(ip) )
-s.send("booyah".encode())
-reply = s.recv(4096)
-print(reply)
-s.close()
+class EchoClientProtocol(asyncio.Protocol):
+    def __init__(self, loop):
+        self.loop = loop
+
+    def connection_made(self, transport):
+        # transport.write(self.message.encode())
+        # print('Data sent: {!r}'.format(self.message))
+        print("Connection Made")
+        a = self.loop.ensure_future(listen_for_input())
+        self.loop.run_until_complete(a)
+
+    async def listen_for_input(self):
+        while True:
+            await input()
+
+    def data_received(self, data):
+        print('Data received: {!r}'.format(data.decode()))
+
+    def connection_lost(self, exc):
+        print('The server closed the connection')
+        print('Stop the event lop')
+        self.loop.stop()
+
+loop = asyncio.get_event_loop()
+coro = loop.create_connection(lambda: EchoClientProtocol(loop),
+                              'localhost', 8888)
+loop.run_until_complete(coro)
+loop.run_forever()
+loop.close()
--- server.py
+++ server.py
@@ -1,12 +1,31 @@
-import socket
-HOST=''
-PORT = 8888
-s=socket.socket()
-s.bind((HOST,PORT))
-print('bound socket')
-s.listen(10)
-conn, addr = s.accept()
-text = conn.recv(2096)
-print(text)
-conn.sendall('message from server...'.encode())
-print('connected successfully with {}{}'.format(addr[0], addr[1]))
+import asyncio
+
+class EchoServerClientProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
+        self.transport = transport
+
+    def data_received(self, data):
+        message = data.decode()
+        print('Data received: {!r}'.format(message))
+
+        print('Send: {!r}'.format(message))
+        self.transport.write(data + 'oh yeahhhhh'.encode())
+
+loop = asyncio.get_event_loop()
+# Each client connection will create a new protocol instance
+coro = loop.create_server(EchoServerClientProtocol, 'localhost', 8888)
+server = loop.run_until_complete(coro)
+
+# Serve requests until Ctrl+C is pressed
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+# Close the server
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
